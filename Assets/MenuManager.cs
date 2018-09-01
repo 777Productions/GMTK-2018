6ad149dfbac126d1ae824494672ca1db0d1b00d9
Jeremy Sapsed <jms737800@gmail.com>
2018-09-01 16:12:12 +0100

    Menus

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    public Image fadeImage;
+
+    [Range(0, 5)]
+    public float fadeDuration = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FadeHelper.FadeToZeroAlpha(fadeImage, fadeDuration));
+    }
+
+    public void OnStartClicked()
+    {
+        StartCoroutine(FadeHelper.FadeToFullAlpha(fadeImage, fadeDuration, true));
+    }
+}
---
+++ Assets/Scripts/SplashManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour {
+
+    [Range(0.1f, 10.0f)]
+    public float duration = 5.0f;
+
+    [Range(0, 5)]
+    public float fadeDuration = 2.0f;
+
+    public Image fadeImage;
+
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+        StartCoroutine(FadeHelper.FadeToZeroAlpha(fadeImage, fadeDuration));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime >= duration)
+        {
+            StartCoroutine(FadeHelper.FadeToFullAlpha(fadeImage, fadeDuration, true));
+        }
+    }
+}
+
+public static class FadeHelper
+{
+    public static IEnumerator FadeToFullAlpha(Image fadeImage, float fadeDuration, bool loadNextScene = false)
+    {
+        fadeImage.enabled = true;
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, 0);
+
+        while (fadeImage.color.a < 1.0f)
+        {
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fadeImage.color.a + (Time.deltaTime / fadeDuration));
+            yield return null;
+        }
+
+        if (loadNextScene)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public static IEnumerator FadeToZeroAlpha(Image fadeImage, float fadeDuration)
+    {
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, 1);
+
+        while (fadeImage.color.a > 0.0f)
+        {
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fadeImage.color.a - (Time.deltaTime / fadeDuration));
+            yield return null;
+        }
+
+        fadeImage.enabled = false;
+    }
+}
