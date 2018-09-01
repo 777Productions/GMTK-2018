502fc41b79b50d2aec0c6cd30c1960149d809bbf
Jeremy Sapsed <jms737800@gmail.com>
2018-09-01 20:09:15 +0100

    Fixed order layer of snow and replaced play button with key command

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class MenuManager : MonoBehaviour {
 
@@ -16,7 +17,15 @@ public class MenuManager : MonoBehaviour {
         StartCoroutine(FadeHelper.FadeToZeroAlpha(fadeImage, fadeDuration));
     }
 
-    public void OnStartClicked()
+    private void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Player1_Grab"))
+        {
+            OnPlay();
+        }
+    }
+
+    public void OnPlay()
     {
         StartCoroutine(FadeHelper.FadeToFullAlpha(fadeImage, fadeDuration, true));
     }
